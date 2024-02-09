8fd6612bf83cb93e75c257b4a282017ff7d936a3
Erenyeager0215 <zlin08022@gmail.com>
2024-02-09 22:31:49 +0900

    サイドバーの作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadsetOffIcon from "@mui/icons-material/HeadsetOff";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 
 const Sidebar = () => {
   return (
@@ -19,6 +24,36 @@ const Sidebar = () => {
         <div className="sidebarTop">
           <h1>Discord</h1>
           <ExpandMoreIcon />
+        </div>
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>ふなばしごはんちゃんねる</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+          <div className="sidebarFooter">
+            <div className="sidebarAccont">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>やまだたろう</h4>
+                <span>#123456</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadsetOffIcon />
+              <SettingsRoundedIcon />
+            </div>
+          </div>
         </div>
       </div>
     </div>
